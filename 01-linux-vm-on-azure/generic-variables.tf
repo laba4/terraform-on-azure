6541398c2f76aff47ac7eabd41a1f8fd4261d151
laba4 <328246425+laba4@users.noreply.github.com>
2026-09-16 18:24:50 +0200

    feat: Adds generic variables and computed locals

---
+++ 01-linux-vm-on-azure/generic-variables.tf
@@ -0,0 +1,17 @@
+variable "business_divsion" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-default"
+}
+
+variable "resource_group_location" {
+  type    = string
+  default = "switzerlandnorth"
+}
---
+++ 01-linux-vm-on-azure/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  resource_name_prefix = "${var.business_divsion}-${var.environment}"
+} 
