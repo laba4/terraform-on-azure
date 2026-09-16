c174deb2dc891fb0657a7c4b368ff44a69c521b4
laba4 <328246425+laba4@users.noreply.github.com>
2026-09-16 18:29:16 +0200

    feat: Adds network-specific variables and virtual network

---
+++ 01-linux-vm-on-azure/vnet-variables.tf
@@ -0,0 +1,29 @@
+variable "vnet_name" {
+  type    = string
+  default = "vnet-default"
+}
+
+variable "vnet_address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "vm_subnet_name" {
+  type    = string
+  default = "vmsubnet"
+}
+
+variable "vm_subnet_address" {
+  type    = list(string)
+  default = ["10.0.1.0/24"]
+}
+
+variable "bastion_subnet_name" {
+  type    = string
+  default = "bastionsubnet"
+}
+
+variable "bastion_subnet_address" {
+  type    = list(string)
+  default = ["10.0.100.0/24"]
+}
