e86a275e7455d7df2be4c91162313ee2b83cc87b
laba4 <328246425+laba4@users.noreply.github.com>
2026-09-17 09:32:05 +0200

    feat: Adds NAT gateway for VM internet outbound connectivity

---
+++ 01-linux-vm-on-azure/nat-gateway.tf
@@ -0,0 +1,24 @@
+resource "azurerm_public_ip" "nat" {
+  name                = "pip-nat"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway" "main" {
+  name                = "nat-main"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "main" {
+  nat_gateway_id       = azurerm_nat_gateway.main.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "vm" {
+  subnet_id      = azurerm_subnet.vm_subnet.id
+  nat_gateway_id = azurerm_nat_gateway.main.id
+}
